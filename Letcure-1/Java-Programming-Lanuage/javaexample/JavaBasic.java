96d55f50f66ee593ea5a10b2ec7532ca2be1d5b2
Shrishail <shrishailchincholi306@gmail.com>
2026-02-10 23:41:52 +0530

    Variable in Java

---
+++ Letcure-1/Java-Programming-Lanuage/javaexample/JavaBasic.java
@@ -0,0 +1,32 @@
+public class  JavaBasic {
+    public static void main(String[] args) {
+        // System.out.print("Hello World \n");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+       
+        int a = 5;
+        int b = 8;
+        System.out.println(a);
+        System.out.println(b);
+        String Name = "John";
+        System.out.println(Name);
+    }
+}
+
+
+
+
+// Output:-1 
+// Hello World
+
+// OutPut2
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+
+// Output:-3
+// 5
+// 8
+// John
