537ee0bcdc097917d952e0aca8a1498c63f215cd
Shrishail <shrishailchincholi306@gmail.com>
2026-02-10 23:54:20 +0530

    Revert "Patter *"
    This reverts commit ca047b8caf0f116fc357dd257426874d6b5868f8.

--- Letcure-1/Java-Programming-Lanuage/javaexample/JavaBasic.java
+++ Letcure-1/Java-Programming-Lanuage/javaexample/JavaBasic.java
@@ -4,14 +4,6 @@ public class  JavaBasic {
         // System.out.println("Hello World");
         // System.out.println("Hello World");
         // System.out.println("Hello World");
-
-            
-        int a = 5;
-        int b = 8;
-        System.out.println(a);
-        System.out.println(b);
-        String Name = "John";
-        System.out.println(Name);
     }
 }
 
@@ -24,8 +16,3 @@ public class  JavaBasic {
 // Hello World
 // Hello World
 // Hello World
-
-// Output:-3
-// 5
-// 8
-// John
