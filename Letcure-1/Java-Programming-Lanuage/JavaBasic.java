bdfabd92018696a5a0d2556a1d2be74b550941a9
Shrishail <shrishailchincholi306@gmail.com>
2026-02-10 23:30:29 +0530

    Java First Code

--- Letcure-1/Java-Programming-Lanuage/JavaBasic.java
+++ Letcure-1/Java-Programming-Lanuage/JavaBasic.java
@@ -1,8 +1,17 @@
 public class  JavaBasic {
     public static void main(String[] args) {
+        System.out.print("Hello World \n");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
         System.out.println("Hello World");
     }
 }
 
-// Output
+// Output:-1 
+// Hello World
+
+// OutPut2
+// Hello World
+// Hello World
+// Hello World
 // Hello World
