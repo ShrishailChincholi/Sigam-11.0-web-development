f46e7371ec6e8d8011ca70669c7360224a9fe8f5
Shrishail <shrishailchincholi306@gmail.com>
2026-02-12 23:34:31 +0530

    Input in java

--- Letcure-1/Java-Programming-Lanuage/javaexample/JavaBasic.java
+++ Letcure-1/Java-Programming-Lanuage/javaexample/JavaBasic.java
@@ -1,3 +1,5 @@
+
+import java.util.Scanner;
 public class  JavaBasic {
     public static void main(String[] args) {
         System.out.print("Hello World \n");
@@ -16,3 +18,23 @@ public class  JavaBasic {
 // Hello World
 // Hello World
 // Hello World
+
+
+//  Input in Java
+//  Scanner sc = new Scanner(System.in);
+// import java.util.Scanner;
+
+
+class Input{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next(); // sc.next() cannot read spaces in input
+
+        System.out.println(input);
+
+        String FullName = sc.nextLine();  // sc.nextLine() reads the entire line of text and allows spaces between words
+
+        System.out.println(FullName);
+    }
+}
+
