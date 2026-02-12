fd41e1c8025cb2df61d067366c65b67f079f30ea
Shrishail <shrishailchincholi306@gmail.com>
2026-02-12 23:12:52 +0530

    DataType

---
+++ Letcure-1/Java-Programming-Lanuage/javaexample/Datatypes.java
@@ -0,0 +1,15 @@
+// DataType in  Java
+public class Datatypes{
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price =  10.5f;
+        int num = 89;
+        System.out.println(price);
+        System.out.println(num);
+    }
+}
