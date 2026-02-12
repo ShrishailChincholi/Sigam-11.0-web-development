fedbae3d5154bfdbf0eaf04ff27a5e9dd673c9a3
Shrishail <shrishailchincholi306@gmail.com>
2026-02-12 23:20:03 +0530

    example

--- Letcure-1/Java-Programming-Lanuage/javaexample/Datatypes.java
+++ Letcure-1/Java-Programming-Lanuage/javaexample/Datatypes.java
@@ -1,5 +1,7 @@
 // DataType in  Java
-public class Datatypes{
+
+public class Datatypes {
+
     public static void main(String[] args) {
         byte b = 8;
         System.out.println(b);
@@ -7,9 +9,21 @@ public class Datatypes{
         System.out.println(ch);
         boolean var = true;
         System.out.println(var);
-        float price =  10.5f;
+        float price = 10.5f;
         int num = 89;
         System.out.println(price);
         System.out.println(num);
     }
 }
+
+// Sum of Number
+class SumofTwo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+
