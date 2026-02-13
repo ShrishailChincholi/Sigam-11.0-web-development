c078900a902c6cd720cee8555b7e2d61c25fb88d
Shrishail <shrishailchincholi306@gmail.com>
2026-02-13 23:11:38 +0530

    Operators

---
+++ Letcure-1/Java-Programming-Lanuage/Operators.java
@@ -0,0 +1,21 @@
+public  class Operators{
+    public static void main(String[] args) {
+        //  Arithmatic Operator 
+        int a = 10;
+        int b = 40;
+        System.out.println("add = "+(a+b));
+        System.out.println("add = "+(a-b));
+        System.out.println("add = "+(a*b));
+        System.out.println("add = "+(a/b));
+        System.out.println("add = "+(a%b));
+
+
+        // Relational Operators
+        int n1 = 10;
+        int n2 = 10;
+        System.err.println(n1 == n2);
+        System.err.println(n1 != n2);
+        System.err.println(n1 >= n2);
+        System.err.println(n1 <= n2);
+    }
+}
