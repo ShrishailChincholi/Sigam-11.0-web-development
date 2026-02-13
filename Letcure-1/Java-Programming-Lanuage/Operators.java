0ef6b74d92d75a93b77c2833b89af8756c3f6a9b
Shrishail <shrishailchincholi306@gmail.com>
2026-02-13 23:21:55 +0530

    Operators

--- Letcure-1/Java-Programming-Lanuage/Operators.java
+++ Letcure-1/Java-Programming-Lanuage/Operators.java
@@ -17,5 +17,29 @@ public  class Operators{
         System.err.println(n1 != n2);
         System.err.println(n1 >= n2);
         System.err.println(n1 <= n2);
+
+        // Logical Operators
+        System.err.println("AND statment "+((3<2) && (5>0)));
+        System.err.println("OR statment" +((3<2) || (5>0)));
+        System.err.println("NOT statment "+!(3<2));
+        
+        
+
     }
 }
+
+/*
+Output
+add = 50
+add = -30
+add = 400
+add = 0
+add = 10
+true
+false
+true
+true
+AND statment false
+OR statmenttrue
+NOT statment true
+*/
