594da5c10b457528f5470ee5bc0bc1bed4e4a2d6
Shrishail <shrishailchincholi306@gmail.com>
2026-02-15 23:19:54 +0530

    for loop

--- Letcure-1/Java-Programming-Lanuage/forLoop.java
+++ Letcure-1/Java-Programming-Lanuage/forLoop.java
@@ -1,9 +1,29 @@
 // For - Loop
 
-public class forLoop{
-    public static void main(String[] args) {
-        for (int  i = 1; i < 10; i++) {
-            System.err.println("Hello world");
+public class forLoop {
+
+    /* For -loop Syntax
+     public static void main(String[] args) {
+         for (int  i = 1; i < 10; i++) {
+             System.out.println("Hello world");
+         }
+    }*/
+    // Square pattern
+    /*
+    public static void main(String[] args) {     
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("****");
         }
+    } */
+// Print Reverse of a number
+    public static void main(String[] args) {
+        int n = 10837;
+        while (n>0) { 
+            int lastdigit = n % 10;
+            System.out.print(lastdigit+" ");
+            n = n/10;
+        }
+        System.out.println();
     }
+
 }
