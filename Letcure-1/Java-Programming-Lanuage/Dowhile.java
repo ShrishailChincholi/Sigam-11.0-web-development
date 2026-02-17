9ee36a6c4bfd75d516eee9a9b2bf051fef1808a0
Shrishail <shrishailchincholi306@gmail.com>
2026-02-17 21:37:30 +0530

    do while

---
+++ Letcure-1/Java-Programming-Lanuage/Dowhile.java
@@ -0,0 +1,12 @@
+//  do while and Break statement
+public class Dowhile {
+
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.println("Hello world");
+            counter++;
+        } while (counter <= 10);
+    }
+
+}
