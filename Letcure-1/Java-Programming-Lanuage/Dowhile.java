6c6bac159b52e4beced7b61254aa0ed9bdaf3947
Shrishail <shrishailchincholi306@gmail.com>
2026-02-17 21:51:42 +0530

    Break & continue keyword

--- Letcure-1/Java-Programming-Lanuage/Dowhile.java
+++ Letcure-1/Java-Programming-Lanuage/Dowhile.java
@@ -1,6 +1,8 @@
-//  do while and Break statement
+
 public class Dowhile {
 
+// do while 
+/*
     public static void main(String[] args) {
         int counter = 1;
         do {
@@ -8,5 +10,48 @@ public class Dowhile {
             counter++;
         } while (counter <= 10);
     }
+    */
+
+//  Break 
+/*
+public static void main(String[] args) {
+    for (int i = 1; i <+5; i++){ 
+        if (i == 3){
+            break;
+        }
+    }
+    System.out.println(" I am out of the loop ");
+    
+}
+ */
+
+//  Using Break 
+
+/*
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    do{
+        System.out.println("Enter Your Number : ");
+        int n = sc.nextInt();
+        if(n % 10 == 0){
+            break;
+        }
+    }while (true);
+
+    
+}
+*/
+
+//  Continue Statement
+
+public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+        if(i == 3){
+            continue;
+        }
+        System.out.println(i);
+    }
+}
+
 
 }
