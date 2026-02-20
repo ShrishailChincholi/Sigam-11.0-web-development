8a573a639a97084f69d02136f68afefa5a8a27d6
Shrishail <shrishailchincholi306@gmail.com>
2026-02-20 21:36:56 +0530

    Pattern in java examples

--- Letcure-1/Java-Programming-Lanuage/Pattern.java
+++ Letcure-1/Java-Programming-Lanuage/Pattern.java
@@ -73,6 +73,26 @@ class HPPI {
 // 12
 // 1
 
+// Print Character Patter
+class charcter {
+
+    static void charPatter() {
+        char ch = 'a';
+        int n = 4;
+        for (int line = 1; line <= n; line++) {
+            for (int number = 1; number <= line; number++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
+// Output
+// a
+// bc
+// def
+// ghij
 
 public class Pattern {
 
@@ -85,12 +105,16 @@ public class Pattern {
         Inverted.Invertedpattern();
         System.out.println();
 
-        System.out.println("half-pyramid  Pattern * :-");
+        System.out.println("half-pyramid  :-");
         HPP.halfppattern();
         System.out.println();
 
-        System.out.println(" Inverted half-pyramid  Pattern * :-");
+        System.out.println(" Inverted half-pyramid   :-");
         HPPI.halfppatternInvorted();
+        System.out.println();
+
+        System.out.println("Print Character Patter :-");
+        charcter.charPatter();
         System.out.println();
 
     }
