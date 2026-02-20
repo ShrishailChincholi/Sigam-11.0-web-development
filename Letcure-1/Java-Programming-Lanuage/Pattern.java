6371f88b73634150154ca737f544d0604b499926
Shrishail <shrishailchincholi306@gmail.com>
2026-02-20 21:09:45 +0530

    Pattern in java examples

--- Letcure-1/Java-Programming-Lanuage/Pattern.java
+++ Letcure-1/Java-Programming-Lanuage/Pattern.java
@@ -2,9 +2,9 @@ class star{
     static void printpattern(){
         for (int line = 1; line <=4; line++) {
             for (int j = 1; j <=line; j++) {
-                System.err.print("*");
+                System.out.print("*");
             }
-            System.err.println();
+            System.out.println();
         }
     }
 }
@@ -15,8 +15,32 @@ class star{
 // ****
 
 
+// Inverted-star pattern
+class Inverted{
+    static void Invertedpattern(){
+        int n =4;
+        for (int line = 1; line <=n; line++) {
+            for (int j = 1; j <=n-line+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+// Output
+// ****
+// ***
+// **
+// *
+
+
+
 public  class  Pattern{
     public static void main(String[] args) {
+        System.out.println("Pattern * :-");
         star.printpattern();
+        System.out.println();
+        System.out.println("Inverted Pattern * :-");
+        Inverted.Invertedpattern();
     }
 }
