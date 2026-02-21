151f98fca0949536bab0010b1ee04bf0a364bfc2
Shrishail <shrishailchincholi306@gmail.com>
2026-02-21 23:21:06 +0530

    Funcation

--- Letcure-1/Java-Programming-Lanuage/Funaction.java
+++ Letcure-1/Java-Programming-Lanuage/Funaction.java
@@ -32,6 +32,15 @@ public class Funaction{
         return f;
     }
 
+    // Binomial Coefficient
+    public static int Binomial(int n , int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+        int binceff = fact_n / (fact_r * fact_nmr);
+        return binceff;
+    }
+
 
     public static void main(String args[]) {
         printhelloworld();
@@ -51,6 +60,13 @@ public class Funaction{
         System.out.println("Enter factoirla Number ");
         int f = sc.nextInt();
         System.out.println("Number of fact:- "+factorial(f));
+
+        // Binomial Coefficient
+        System.out.println("Enter a n value and r value");
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int BC= Binomial(n, r);
+        System.out.println(" Binomial Cofficient :- "+BC);
     }
 }
 
