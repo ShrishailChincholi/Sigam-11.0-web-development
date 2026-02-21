390de42dc5eb80f08c5139dc1ca2ef5fd87fc959
Shrishail <shrishailchincholi306@gmail.com>
2026-02-21 09:52:47 +0530

    Funcation

---
+++ Letcure-1/Java-Programming-Lanuage/Funaction.java
@@ -0,0 +1,15 @@
+
+
+
+public class Funaction{
+
+    // Funcation Sysntax
+    public static void printhelloworld(){
+        System.out.println("Hello world");
+    }
+
+
+    public static void main(String args[]) {
+        printhelloworld();
+    }
+}
