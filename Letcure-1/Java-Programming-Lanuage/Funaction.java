0d2a3936eeb06682f9affe3f2686abe0a45f7ed8
Shrishail <shrishailchincholi306@gmail.com>
2026-02-21 23:08:03 +0530

    Funcation

--- Letcure-1/Java-Programming-Lanuage/Funaction.java
+++ Letcure-1/Java-Programming-Lanuage/Funaction.java
@@ -1,3 +1,5 @@
+// Funcation :-Function is a block of code that is executed when it is called.
+
 
 import java.util.Scanner;
 
@@ -9,9 +11,25 @@ public class Funaction{
         System.out.println("Hello world");
     }
 
+    // Funaction with paramter or formal parameter
     public static void calculatsum(int a, int b) {
         int sum = a +b;
         System.out.println("Sum i s: "+sum);
+    }
+
+    // Find Product 
+    public static int multiply(int a, int b){
+        int product = a* b;
+        return product;
+    }
+
+    // Factorail 
+    public static int factorial(int n){
+        int f=1;
+        for(int i = 1; i<=n; i++){
+            f = f*i;
+        }
+        return f;
     }
 
 
@@ -23,5 +41,16 @@ public class Funaction{
         int a = sc.nextInt();
         int b = sc.nextInt();
         calculatsum(a, b);
+
+        // Product Example
+        System.out.println("Product Example ");
+        int prod = multiply(a, b);
+        System.out.println("a * b = " + prod);
+
+        // Factorial Example
+        System.out.println("Enter factoirla Number ");
+        int f = sc.nextInt();
+        System.out.println("Number of fact:- "+factorial(f));
     }
 }
+
