03acd26c9fff041351669dbf0eb45744013501e1
Shrishail <shrishailchincholi306@gmail.com>
2026-02-25 22:58:06 +0530

    java examples

--- Letcure-1/Java-Programming-Lanuage/Paratice.java
+++ Letcure-1/Java-Programming-Lanuage/Paratice.java
@@ -175,3 +175,19 @@ class  MultiTable{
         }
     }
 }
+
+class Arrayoperations {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
+
