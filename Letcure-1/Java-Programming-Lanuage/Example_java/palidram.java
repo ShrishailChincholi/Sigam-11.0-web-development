f0566a0d641e8a7bb0a9b22080fd346554d35ed1
Shrishail <shrishailchincholi306@gmail.com>
2026-03-09 21:18:18 +0530

    java partice questions

---
+++ Letcure-1/Java-Programming-Lanuage/Example_java/palidram.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class palidram {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPalindrome(num)) {
+            System.out.println("Number is Palindrome");
+        } else {
+            System.out.println("Number is Not Palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        return original == reverse;
+    }
+}
