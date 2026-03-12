1dd43aa1d825184f92546d7bdff6757843b1e3d7
Shrishail <shrishailchincholi306@gmail.com>
2026-03-12 19:11:36 +0530

    java assignments queautions

--- Letcure-1/Java-Programming-Lanuage/Example_java/assi1.java
+++ Letcure-1/Java-Programming-Lanuage/Example_java/assi1.java
@@ -30,3 +30,34 @@ public class assi1 {
 // Output
 // Input an integer: 1234
 // The sum is 10
+
+
+
+ class ArmstrongCheck {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isArmstrong(num)) {
+            System.out.println(num + " is an Armstrong number");
+        } else {
+            System.out.println(num + " is not an Armstrong number");
+        }
+    }
+
+    public static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n = n / 10;
+        }
+
+        return sum == original;
+    }
+}
