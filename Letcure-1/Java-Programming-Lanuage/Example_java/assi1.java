6c7461e7691f5ddbd32834b22495c9fc415fe6c3
Shrishail <shrishailchincholi306@gmail.com>
2026-03-12 19:06:44 +0530

    java assignments queautions

---
+++ Letcure-1/Java-Programming-Lanuage/Example_java/assi1.java
@@ -0,0 +1,32 @@
+// Write a Java method to compute the sum of the digits in an integer.
+
+
+import java.util.Scanner;
+
+public class assi1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input an integer: ");
+        int digits = sc.nextInt();
+
+        System.out.println("The sum is " + sumDigits(digits));
+    }
+
+    public static int sumDigits(int n) {
+        int sumOfDigits = 0;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            sumOfDigits += lastDigit;
+            n /= 10;
+        }
+
+        return sumOfDigits;
+    }
+}
+
+// Output
+// Input an integer: 1234
+// The sum is 10
