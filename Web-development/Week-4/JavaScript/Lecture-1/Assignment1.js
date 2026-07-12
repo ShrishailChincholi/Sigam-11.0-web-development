9b5b0234f5433414b3f58d44365827871c69b06c
Shrishail <shrishailchincholi306@gmail.com>
2026-07-12 23:30:36 +0530

    javascript assignment 1

---
+++ Web-development/Week-4/JavaScript/Lecture-1/Assignment1.js
@@ -0,0 +1,20 @@
+// 1. Declare your name as a string and print its length.
+let name = "Shrishail";
+console.log("Length of name:", name.length);
+
+// 2. Declare your first name as a string and print its first character.
+let firstName = "Shrishail";
+console.log("First character:", firstName[0]);
+
+// 3. Declare your first name as a string and print its last character.
+console.log("Last character:", firstName[firstName.length - 1]);
+
+// 4. What is the output of the following code?
+console.log("apnacollege" + 123);
+
+// 5. What are the lengths of an empty string and a string with a single space?
+let emptyString = "";
+let singleSpace = " ";
+
+console.log("Length of empty string:", emptyString.length);
+console.log("Length of single space string:", singleSpace.length);
